1b2e146fe3145c1e0d0ff57df543c350e776de88
Death <ricardo.pajaro.bc2d@gmail.com>
2022-11-28 13:14:14 -0600

    refactor details screen

--- lib/screens/details/widgets/title_site.dart
+++ lib/screens/details/widgets/title_site.dart
@@ -7,36 +7,58 @@ class TitleSite extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Text(
-            "Oeschinen Lake CampGrouend",
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 8),
-          Text(
-            "Kandersteg, Switzerland",
-            style: Theme.of(context).textTheme.caption,
-          ),
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: const [_TitleAndDescription(), _IconSite()],
       ),
-      trailing: SizedBox(
-        width: 40,
-        child: Row(
-          children: const [
-            Icon(
-              Icons.star_outlined,
-              color: Colors.red,
-            ),
-            Text("41")
-          ],
+    );
+  }
+}
+
+class _IconSite extends StatelessWidget {
+  const _IconSite({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        Icon(
+          Icons.star_outlined,
+          color: Colors.red,
         ),
-      ),
+        Text("41")
+      ],
+    );
+  }
+}
+
+class _TitleAndDescription extends StatelessWidget {
+  const _TitleAndDescription({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          "Oeschinen Lake CampGrouend",
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(height: 8),
+        Text(
+          "Kandersteg, Switzerland",
+          style: Theme.of(context).textTheme.caption,
+        ),
+      ],
     );
   }
 }
